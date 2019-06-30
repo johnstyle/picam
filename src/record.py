77c886331bebc94d0d706410451f03a3e0aea954
Jonathan <1837566+johnstyle@users.noreply.github.com>
2019-06-30 16:18:03 +0200

    Update record.py

--- src/record.py
+++ src/record.py
@@ -5,14 +5,13 @@ import datetime
 import os
 
 ROOT_PATH = "/var/www/html"
-SEGMENT_TIME = 60
-ENCODING = "copy"
+SEGMENT_TIME = 30
 
 recording_path = os.path.join(ROOT_PATH, datetime.datetime.now().strftime("%Y%m%d"))
 os.mkdir(recording_path)
 
 segments_path = os.path.join(recording_path, "{}_%03d.avi".format(datetime.datetime.now().strftime("%H%M%S")))
 
-command = "ffmpeg -i /dev/video0 -c:v {} -an -sn -dn -segment_time {} -f segment {}".format(ENCODING, SEGMENT_TIME, segments_path)
+command = "ffmpeg -i /dev/video0 -r 30 -c:v copy -an -sn -dn -vf drawtext=\"fontfile=Arial.ttf:fontcolor=white: timecode='00\:00\:00;00': r='30000/1001': text='': fontsize=148: x=190: y=260:\" -segment_time {} -f segment {}".format(SEGMENT_TIME, segments_path)
 
 subprocess.call(command, shell=True)
